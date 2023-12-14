d8f22f20279903335f2a964fc900822e2847149e
Julia Nezhelskai <yulyazelikova@gmail.com>
2023-12-14 22:03:56 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
 import React, { useState } from "react";
 import "./App.css";
-import { DisplayRest } from "./components/DisplayRest";
-import { DisplayChangeVAlue } from './components/DisplayChangeVAlue';
 import { Display } from "./components/Display";
+import { DisplayChangeVAlue } from './components/DisplayChangeVAlue';
 
-
-const recetonClick = (startValue: number, setValueInintial: (num: number) => void) => setValueInintial(startValue);
+type ErrorType =  'Inncorrect Value!' | 'PleaseClik' | false
 
 function App() {
-  // const [tasks, setTasks] = useState([
-  //   { id: crypto.randomUUID(), title: "hello", isDone: false },
-  // ]);
 
   const [maxValue, setMaxValue] = useState(5);
   const [minValue, setMinValue] = useState(0);
-  const [error, setError] = useState<boolean>(false)
+  const [error, setError] = useState<ErrorType>(false)
 
   const [value, setValue] = useState<number>(minValue);
-
-  // React.useEffect(() => {
-  //   let valueLocalstore = localStorage.getItem('valueeee')
-  //   if (valueLocalstore) {
-  //     let newValueLoczlStore = JSON.parse(valueLocalstore)
-  //     setValue(newValueLoczlStore)
-  //   }
-  // }, [])
-
-  // React.useEffect(() => {
-  //   localStorage.setItem('valueeee', JSON.stringify(value))
-  // }, [value])
-
 
   const chanchValue = () => {
     if (value < maxValue) {
@@ -37,61 +19,22 @@ function App() {
     }
   };
 
-
   const recetonClickBtn = () => setValue(minValue);
 
   const disabledInc = value >= maxValue;
   const disabledRecet = value === minValue;
 
-  const setLocalStoreHendler = () => {
-    localStorage.setItem('value+1', JSON.stringify(value + 1))
-  }
 
-  const clearLocalStoreHendler = () => {
-    localStorage.clear()
-  }
-
-  const removeLocalStoreHendler = () => {
-    localStorage.removeItem('value+1')
-  }
-
-  const getLocalStoreHendler = () => {
-    let valueLocalstore = localStorage.getItem('valueeee')
-    if (valueLocalstore) {
-      let newValueLoczlStore = JSON.parse(valueLocalstore)
-      setValue(newValueLoczlStore)
-    }
-  }
-
-  const changeMaxValueClisar = (setMaxValue: (num: number) => void, maxValue: number) => {
-    setMaxValue(maxValue + 1)
-  }
-
-  const changeMaxValue = () => {
-    if (maxValue >= minValue) {
-      changeMaxValueClisar(setMaxValue, maxValue)
-    }
-  }
-  const changeMinValue = () => {
-    if (minValue < maxValue) {
-      setMinValue(minValue + 1)
-    }
-  }
   const changeMaxMinValue = (max: number, min: number) => {
     setMaxValue(max)
     setMinValue(min)
-    recetonClick(min, setValue)
+    setValue(min)
   }
 
   return (
     <div className="App">
-      <Display value={value}
-        maxValue={maxValue}
-        minValue={minValue}
-        changeMaxValue={changeMaxValue}
-        changeMinValue={changeMinValue} />
 
-      <DisplayRest
+      <Display
         recetonClickBtn={recetonClickBtn}
         chanchValue={chanchValue}
         disabledInc={disabledInc}
@@ -107,29 +50,6 @@ function App() {
         maxValue={maxValue}
         minValue={minValue}
       />
-
-      {/* <div className="container">
-
-        <button onClick={setLocalStoreHendler}>set</button>
-        <button onClick={getLocalStoreHendler}>get</button>
-        <button onClick={clearLocalStoreHendler}>clear</button>
-        <button onClick={removeLocalStoreHendler}>remove</button>
-        <Button
-          name="inc"
-          classStyle="buttpn-rest"
-          callBAckClick={chanchValue}
-          isDisabled={disabledInc}
-        />
-        <Button
-          name="recet"
-          classStyle="buttpn-recet"
-          callBAckClick={recetonClickBtn}
-          isDisabled={disabledRecet}
-        />
-      </div> */}
-      {/* <div className="container">
-        <input type="checkbox" onChange={() => { }} />
-      </div> */}
     </div>
   );
 }
--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,25 +1,50 @@
 import { Button } from "./Button"
 
-type DisplayPropsType = {
-    value: number
-    maxValue: number
-    minValue: number
-    changeMaxValue: () => void
-    changeMinValue: () => void
+type DisplayRestPropsType = {
+  value: number
+  maxValue: number
+  recetonClickBtn: () => void
+  disabledRecet: boolean
+  chanchValue: () => void
+  disabledInc: boolean
+  error: any
 }
 
-export const Display = (props: DisplayPropsType) => {
-    return (
+export const Display: React.FC<DisplayRestPropsType> = ({
+  value,
+  maxValue,
+  recetonClickBtn,
+  disabledRecet,
+  chanchValue,
+  disabledInc,
+  error
+}) => {
+  console.log(error)
 
-        <div className="big-container">
-            <div className="container">
-                {props.maxValue}
-            </div>
-            <div>
-                {props.minValue}
-            </div>
-            <Button name="+value" callBAckClick={props.changeMaxValue} classStyle="d"></Button>
-            <Button name="-value" callBAckClick={props.changeMinValue} classStyle="d"></Button>
-        </div>
-    )
+  const inncorrectValue = <span>{error}</span>
+
+  return (
+
+
+    <div className="big-container">
+      <div className="container">
+        <p className={value === maxValue ? 'value-reset' : 'value'} >
+          {(error === "Inncorrect Value!" || error === "PleaseClik") ? inncorrectValue : value}
+        </p>
+        <Button
+          name="recet"
+          classStyle="buttpn-recet"
+          callBAckClick={recetonClickBtn}
+          isDisabled={disabledRecet}
+        />
+        <Button
+          name="inc"
+          classStyle="buttpn-rest"
+          callBAckClick={chanchValue}
+          isDisabled={disabledInc}
+        />
+      </div>
+    </div>
+
+  )
 }
